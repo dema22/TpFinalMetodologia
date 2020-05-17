b571c405ba31270b5806d78446a05758cd26fa5c
felipe <felipedemaria@hotmail.com>
2020-05-17 17:19:41 -0300

    Creating project with Spring Initializr

---
+++ src/test/java/com/utn/MatafuegosMarplatense/MatafuegosMarplatenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utn.MatafuegosMarplatense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatafuegosMarplatenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
